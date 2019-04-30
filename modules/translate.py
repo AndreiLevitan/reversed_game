935a6cff195f4cc6c4eb7d51f46314f564ae8d43
fedor <swiks321@gmail.com>
2019-04-30 14:08:08 +0400

    Комментирование и описание работы функций работы с Яндекс.Переводчиком модуля translate.py

--- modules/translate.py
+++ modules/translate.py
@@ -1,22 +1,28 @@
 import requests
 
 
+# API ключ к сервису ЯндексюПереводчика
 api_key = 'trnsl.1.1.20190416T153618Z.98dd07147a00d262.de44998b11f26635808dba9425a7c3861743aa18'
 
 
+# Перевод русского текста на английский
 def translate_ru(text):
+    # Сервер API Яндекс.Переводчика
     url = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
+    # Параметры запроса на сервер
     params = {
         'key': api_key,
         'text': text,
         'lang': 'ru-en'
     }
-
+    # Создаем ответ из запроса с параметрами
     response = requests.get(url, params)
     json = response.json()
+    # Возвращаем str перевода text
     return json['text'][0]
 
 
+# Перевод английского текста на русский
 def translate_en(text):
     url = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
     params = {
